3bba4cbda5f78760be92dc304ad9d5511f904f8e
fengziMu119 <1194420200Qqq.com>
2020-04-16 00:19:15 +0800

    栈

--- 2 stack、Queue and Hash/1 实现栈/main.go
+++ 2 stack、Queue and Hash/1 实现栈/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"algorithm/go_type"
 	"errors"
 	"fmt"
 )
 
+// 切片方式实现
 type SliceStack struct {
 	arr       []int
 	stackSize int
@@ -40,7 +42,12 @@ func (s *SliceStack) Push(t int) {
 	s.stackSize++
 }
 
-func main() {
+func SliceMode() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
 	fmt.Println("Slice构建栈结构")
 	sliceStack := &SliceStack{arr: make([]int, 0)}
 	sliceStack.Push(1)
@@ -52,4 +59,66 @@ func main() {
 	fmt.Println("栈的大小为：", sliceStack.Size())
 	fmt.Println("弹出元素为：", sliceStack.Pop())
 	fmt.Println("弹栈成功: ", sliceStack.Size())
+}
+
+// 链表结构实现
+type LinkedStack struct {
+	head *go_type.LNode
+}
+
+func (l *LinkedStack) IsEmptyL() bool {
+	return l.head.Next == nil
+}
+
+func (l *LinkedStack) SizeL() int {
+	size := 0
+	node := l.head.Next
+	for node != nil {
+		node = node.Next
+		size++
+	}
+	return size
+}
+
+func (l *LinkedStack) PushL(e int) {
+	node := &go_type.LNode{
+		Data: e,
+		Next: l.head.Next,
+	}
+	l.head.Next = node
+}
+
+func (l *LinkedStack) PopL() int {
+	tmp := l.head.Next
+	if tmp != nil {
+		l.head.Next = tmp.Next
+		return tmp.Data.(int)
+	}
+	panic(errors.New("栈已经为空！"))
+}
+
+func (l *LinkedStack) TopL() int {
+	if l.head.Next != nil {
+		return l.head.Next.Data.(int)
+	}
+	panic(errors.New("栈已经为空！"))
+}
+
+func LinkedMode() {
+	fmt.Println("Linked构建栈结构")
+	linkedStack := &LinkedStack{head: &go_type.LNode{}}
+	linkedStack.PushL(1)
+	linkedStack.PushL(2)
+	linkedStack.PushL(3)
+	linkedStack.PushL(4)
+	linkedStack.PushL(5)
+	fmt.Println("栈定元素为：", linkedStack.TopL())
+	fmt.Println("栈的大小为：", linkedStack.SizeL())
+	fmt.Println("弹出元素为：", linkedStack.PopL())
+	fmt.Println("弹栈成功: ", linkedStack.SizeL())
+}
+
+func main() {
+	SliceMode()
+	LinkedMode()
 }
