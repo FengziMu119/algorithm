8e4a3987ad0c766987c476bec8447ab5bbcca217
fengziMu119 <1194420200Qqq.com>
2020-04-16 16:12:53 +0800

    队列

--- 2 stack、Queue and Hash/2 实现队列/main.go
+++ 2 stack、Queue and Hash/2 实现队列/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"algorithm/go_type"
 	"errors"
 	"fmt"
 )
@@ -38,7 +39,7 @@ func (s *SliceQueue) GetBack() int {
 }
 
 // 删除队列头元素
-func (s *SliceQueue) delQueue() {
+func (s *SliceQueue) DelQueue() {
 	if s.rear > s.front {
 		s.rear--
 		s.arr = s.arr[1:]
@@ -68,6 +69,76 @@ func SliceMode() {
 	fmt.Println("队列大小为：", sliceQueue.Size())
 }
 
+type LinkedQueue struct {
+	head *go_type.LNode
+	end  *go_type.LNode
+}
+
+// 获取队列中元素的个数
+func (l *LinkedQueue) Size() int {
+	size := 0
+	node := l.head
+	for node != nil {
+		node = node.Next
+		size++
+	}
+	return size
+}
+
+// 入队列
+func (l *LinkedQueue) EnQueue(e int) {
+	node := &go_type.LNode{Data: e}
+	if l.head == nil {
+		l.head = node
+		l.end = node
+	} else {
+		l.end.Next = node
+		l.end = node
+	}
+}
+
+// 出队列
+func (l *LinkedQueue) DelQueue() {
+	if l.head == nil {
+		panic(errors.New("队列已经为空！"))
+	}
+	l.head = l.head.Next
+	if l.head == nil {
+		l.end = nil
+	}
+}
+
+// 取得队列首元素
+func (l *LinkedQueue) GetFront() int {
+	if l.head == nil {
+		panic(errors.New("队列已经为空！"))
+	}
+	return l.head.Data.(int)
+}
+
+//取得队列尾元素
+func (l *LinkedQueue) GetBack() int {
+	if l.end == nil {
+		panic(errors.New("队列已经为空！"))
+	}
+	return l.end.Data.(int)
+}
+func LinkedMode() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	fmt.Println("Linked构建队列结构")
+	linkedQueue := &LinkedQueue{}
+	linkedQueue.EnQueue(1)
+	linkedQueue.EnQueue(2)
+	fmt.Println("队列首元素为：", linkedQueue.GetFront())
+	fmt.Println("队列尾元素为：", linkedQueue.GetBack())
+	fmt.Println("队列大小为：", linkedQueue.Size())
+}
 func main() {
 	SliceMode()
+	LinkedMode()
 }
