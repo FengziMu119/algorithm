940197388668f9a249a4cf2f3add3e6c982934f5
FengziMu119 <1194420200@qq.com>
2020-04-16 21:44:51 +0800

    链表

--- 1 Linked/1 链表逆序/main.go
+++ 1 Linked/1 链表逆序/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// 就地逆序
 func Reverse(node *LNode) {
 	if node == nil || node.Next == nil {
 		return
@@ -22,11 +23,71 @@ func Reverse(node *LNode) {
 	node.Next = pre
 }
 
+// 递归法
+func RecursiveReverseChild(node *LNode) *LNode {
+	if node == nil || node.Next == nil {
+		return node
+	}
+	newHead := RecursiveReverseChild(node.Next)
+	node.Next.Next = node
+	node.Next = nil
+	return newHead
+}
+
+func RecursiveReverse(node *LNode) {
+	firstNode := node.Next
+	// 递归调用
+	newHead := RecursiveReverseChild(firstNode)
+	node.Next = newHead
+}
+
+func ReversePrint(node *LNode) {
+	if node == nil {
+		return
+	}
+	ReversePrint(node.Next)
+	if node.Data != nil {
+		fmt.Print(node.Data, " ")
+	}
+}
+
+// 插入法
+func InsertReverse(node *LNode) {
+	if node == nil || node.Next == nil {
+		return
+	}
+	var cur *LNode  // 当前结点
+	var next *LNode // 后继结点
+	cur = node.Next.Next
+	// 设置链表的第一个结点为尾结点
+	node.Next.Next = nil
+	// 把遍历到的结点插到头结点的后面
+	for cur != nil {
+		next = cur.Next
+		cur.Next = node.Next
+		node.Next = cur
+		cur = next
+	}
+}
+
 func main() {
 	head := &LNode{}
 	fmt.Println("就地逆序：")
 	CreateNode(head, 8)
-	PrintNode("逆序前：", head)
+	PrintNode("就地逆序前：", head)
 	Reverse(head)
-	PrintNode("逆序后：", head)
+	PrintNode("就地逆序后：", head)
+	fmt.Println("递归逆序：")
+	PrintNode("递归逆序前：", head)
+	RecursiveReverse(head)
+	fmt.Print("递归逆序后：")
+	ReversePrint(head)
+	fmt.Println()
+	fmt.Println("插入逆序：")
+	fmt.Print("插入逆序前：")
+	ReversePrint(head)
+	fmt.Println()
+	InsertReverse(head)
+	fmt.Print("插入逆序后：")
+	ReversePrint(head)
 }
